4c7aa307331eb405843caa97f96e949c320e9188
Unknown <leonardo98ti@gmail.com>
2018-11-30 08:06:36 -0200

    Integração com o facebook

--- app/src/main/java/com/example/rodrigo/projetomds/LoginActivity.kt
+++ app/src/main/java/com/example/rodrigo/projetomds/LoginActivity.kt
@@ -1,19 +1,68 @@
 package com.example.rodrigo.projetomds
 
+import android.app.PendingIntent.getActivity
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import java.util.*
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
+import android.widget.Toast
+import com.facebook.FacebookSdk.getApplicationContext
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private var callbackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        login_button.setOnClickListener(){
-            val intent = Intent(this, ListaProdutosActivity::class.java)
-            startActivity(intent)
+
+
+        login_button_facebook.setOnClickListener(){
+            callbackManager = CallbackManager.Factory.create()
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+
+            LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(loginResult: LoginResult) {
+                        Log.d("MainActivity", "Facebook token: " + loginResult.accessToken.token)
+                        val intent = Intent(applicationContext, ListaProdutosActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                    override fun onCancel() {
+                        Log.d("MainActivity", "Facebook onCancel.")
+
+                    }
+
+                    override fun onError(error: FacebookException) {
+                        Log.d("MainActivity", "Facebook onError.")
+
+                    }
+                })
+
         }
+
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager?.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
